dd9f775d55ed5ad68071c7f13cea588b9a22c480
yam <852821931@qq.com>
2025-03-28 14:48:11 +0800

    初始化测试

--- train_task_expert_yam01.sh
+++ train_task_expert_yam01.sh
@@ -43,6 +43,7 @@ TASK="gsm8k" # Available options: mbpp2, gsm8k, ai2_arc, cls
 # Training Setting
 NUM_ITERS=1
 
+# 测试git
 
 # This script needs 2 gpus
 CUDA_VISIBLE_DEVICES=0,1,2,3 python svd_reinforce_hydra_v0.py \
